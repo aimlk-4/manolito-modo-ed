8a1f4c108194e042a80ee0092bf0fa011adca1b7
aimlk-4 <aimarqueromocete@gmail.com>
2024-12-04 21:37:44 +0000

    Actualizar pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,95 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "H0OLA":
+            case "anim1":return [img`
+. . . . . . . f f e e e . . . . 
+. . . . . . f 3 3 3 3 f . . . . 
+. . . . . f 3 3 3 3 3 3 f f . . 
+. . . . f 3 e e e 3 3 e 3 3 . . 
+f . . . f e 3 3 e 3 3 e 3 3 e . 
+1 f 2 . 3 3 d 3 e e 3 d 3 3 3 . 
+1 f 2 . 2 d d d d 3 d d 3 3 e f 
+f 5 2 2 2 3 d d d d d d c c . . 
+1 . 5 5 5 b b b b d d c c f . . 
+1 f f f f f c 3 b c c c f f f . 
+1 1 f . b b c c c c c c c c 3 . 
+. f f . f f f c b b b b c c . . 
+. . 3 . . c c f f f f c c f . . 
+. . . . . f c c b b b c f f . . 
+. . . . . f f c f f b c f . . . 
+. . . . . . f c f c f f . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f e e f . . . . . . 
+. . . . f 3 3 3 3 3 f e e . . . 
+. . . f 3 3 3 3 3 3 3 3 f . . . 
+. . e 3 e 3 3 3 3 3 e 3 3 f . . 
+. . e 3 3 3 e e e e 3 3 3 f . . 
+. e 3 3 d d 3 3 3 3 3 d f f . . 
+. . f 3 d d 5 f f d d b b 3 f . 
+. . . e d 2 5 5 5 f b c b 3 f . 
+. . . f c b 2 5 f c c c 3 . . . 
+. . . . 2 2 2 5 f b c b f . f . 
+. . . . f 2 2 5 f c c b f f 1 f 
+. . . . . b 2 5 f b b f f 1 f . 
+. . . f f b b f f f . f 1 1 f . 
+. . f 1 1 f f 1 1 1 1 1 1 f . . 
+`, img`
+. . . . . e e e f f . . f . . . 
+. . . . f 3 3 3 3 e f f 1 f . . 
+. . . f 3 3 3 3 3 e e e 1 1 f . 
+. . . f e 3 3 3 3 3 e e f 1 1 f 
+. . f 3 3 e e 3 e e 3 3 f f 1 1 
+. e 3 3 3 3 e e e 3 3 3 f . f 1 
+. e 3 3 3 3 3 3 3 3 3 f f f f 1 
+. . e d 3 3 3 3 3 3 f 5 5 5 f 1 
+. . e d d d d d d 2 2 2 2 2 2 f 
+. . f c d d d d d c c 2 2 2 f 1 
+. . f c c c d c c c 2 2 . . f 1 
+. . . f c c c c c b b f . . . f 
+. . . f c f f f b b c f . . . . 
+. . . . f c c b f f f . . . . . 
+. . . . f c b f f c f . . . . . 
+. . . . . f b c b f f . . . . . 
+`, img`
+. . f . f f f 3 f f f f . . . . 
+. f 1 1 1 1 1 f 5 1 1 1 f . . . 
+f 1 1 f f . f f 5 5 f 2 2 . . . 
+f 1 f . . . . f 5 2 2 . . . . . 
+. f . . . . . f 5 5 2 . . . . . 
+. . . . f f f 3 f 5 3 f f . . . 
+3 . . f 3 e e 3 3 e e 3 f f . . 
+. . . f 3 3 3 3 3 3 3 3 3 . . . 
+. . f 3 e 3 3 3 e e 3 3 e f . . 
+. . . 3 3 3 3 3 e 3 3 3 3 f f . 
+. f f d 3 e 3 3 3 3 3 e 3 f . . 
+. . f d d 3 e e e e e 3 f f . . 
+. . f c c 3 3 e e e e 3 f . . . 
+. . . f c f d d d d d f f f . . 
+. . . . f f f f f c f f . . . . 
+. . . . f b c c b f f . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
